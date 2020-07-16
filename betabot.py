5fdf0d957881da59587caa3737f098b54b731869
Ex0ticButterz8 <64514649+Ex0ticButterz8@users.noreply.github.com>
2020-07-16 16:48:02 -0500

    Update Sweep for betabot.py

--- betabot.py
+++ betabot.py
@@ -5,16 +5,26 @@ import os
 import mechanicalsoup
 import getpass
 import argparse
+import time
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '!')
 
+#custom check
+def is_it_me(ctx):
+    return ctx.author.id == 300246955681120256
+
 @client.event
 async def on_ready():
     print('Bot is ready.')
     await client.change_presence(status=discord.Status.online, activity=discord.Game('and making your quarantine happier!'))
+    time.sleep(5)
     print('Activity & Game Displayed')
+    time.sleep(5)
+    print("--------------------------------------------\n           Ex0tic Boi Discord Bot\n            v. 0.1.1 Beta\n--------------------------------------------")
 
 @client.command()
 async def sadness(ctx):
@@ -29,13 +39,9 @@ async def _8ball(ctx, *, question):
 	await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 @client.command()
-@commands.has_permissions(manage_messages=True)
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
-
-#custom check
-def is_it_me(ctx):
-    return ctx.author.id == 300246955681120256
 
 @client.command()
 async def Hello(ctx):
@@ -45,9 +51,9 @@ async def Hello(ctx):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-for filename in os.listdir('B:\PythonAMD64\cogs'):
+for filename in os.listdir(r'B:\PythonAMD64\BetaCogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+        client.load_extension(f'BetaCogs.{filename[:-3]}')
 
 
-client.run('Discord_Bot_Code')
+client.run('Discord Bot Code')
